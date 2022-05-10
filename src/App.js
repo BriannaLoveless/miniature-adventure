ff7451a831b59d53b11a88c2328e5837c3ef01d5
Brianna Loveless <briannabel97@gmail.com>
2022-05-10 14:31:27 -0500

    dice rollers

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
+
       <h1>Miniature Adventure</h1>
       <div className='card'>
         <h2>Monster!</h2>
-        <div className="card-content-wrapper">
+        <div className="card-content-container">
         <div className='card-img'></div>
         <div className="card-challenge-container">
         <div className='card-challenge-box'>Num</div>
@@ -17,6 +18,38 @@ function App() {
         <div className='card-challenge-box'>Num</div>
         </div>
         </div>
+      </div>
+
+      <div className="dice-container">
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        <div className="die-button-container">
+        <div className="die"></div>
+        <button>Roll!</button>
+        </div>
+        
 
       </div>
     </div>
