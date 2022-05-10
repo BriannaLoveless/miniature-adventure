42b093256c41551ed53b3cb014eb6c4b2dd8f6e9
Brianna Loveless <briannabel97@gmail.com>
2022-05-10 14:16:19 -0500

    very basic hard coded card

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import React from 'react';
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>Miniature Adventure</h1>
+      <div className='card'>
+        <h2>Monster!</h2>
+        <div className="card-content-wrapper">
+        <div className='card-img'></div>
+        <div className="card-challenge-container">
+        <div className='card-challenge-box'>Num</div>
+        <div className='card-challenge-box'>Num</div>
+        <div className='card-challenge-box'>Num</div>
+        <div className='card-challenge-box'>Num</div>
+        <div className='card-challenge-box'>Num</div>
+        <div className='card-challenge-box'>Num</div>
+        </div>
+        </div>
+
+      </div>
     </div>
   );
 }
